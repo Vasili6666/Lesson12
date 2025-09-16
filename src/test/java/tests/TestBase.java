f4a1decf3c55ec636c8820608aec542ecc7866f8
Vasili Pl <w2e3r4t51982@gmail.com>
2025-09-16 10:39:39 +0200

    variables are moved  to the TestBase.java

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -12,12 +12,12 @@ public class TestBase {
     public static final String REMOTE_DRIVER_URL = System.getProperty("remoteDriverUrl", "selenoid.autotests.cloud");
     public static final int TIMEOUT = Integer.parseInt(System.getProperty("timeout", "5000"));
 
-    @BeforeAll
+   /* @BeforeAll
     static void beforeAll() {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
         Configuration.timeout = 10000;
 
-    }
+    }*/
 
 }
